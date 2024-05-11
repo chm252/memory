7b225081e65cbcf8f2733c709eb826e69c928e1e
chm252 <chinhongming252@gmail.com>
2024-05-12 00:18:04 +0800

    Edit

---
+++ src/DifficultySelection.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './DifficultySelection.css'
+function DifficultySelection(props) {
+
+    const[showDifficulty, setShowDifficulty] = useState(false);
+    const levels = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+    let [selected, setSelected] = useState(20);
+    return (
+        <div style = {{display:'flex'}}>
+            {showDifficulty 
+                && 
+                <>
+            {levels.map((level, index) => (
+                <button style={level==selected ? {borderColor :'green', borderWidth:'thick'} : {borderColor :'white'}} key={index} className='level' 
+                    onClick={() => {
+                        if (level != selected) {
+                            setSelected(level);
+                            props.difficultyChange(level)
+                        }
+                }}>
+                    {level}
+                </button>
+            ))}
+                </>}
+            <button className="DS" onClick={()=>setShowDifficulty(!showDifficulty)}/>
+        </div>
+    )
+}
+
+export default DifficultySelection;
--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,28 @@
 import './Main.css';
 import Display from './Display.js'
+import DS from './DifficultySelection.js'
 import { useState } from 'react';
 function Main () {
     
     const [key, setKey] = useState(0);
     const handleRestart = () => setKey(key + 1);
-
+    const [level, setLevel] = useState(20);
+    const difficultyChange = (level) => {
+        handleRestart();
+        setLevel(level);
+    }
     let string = ""
-    for (let i = 0 ; i < 20  ; i++) {
+    for (let i = 0 ; i < level  ; i++) {
             let random = Math.floor(Math.random() * 10);
             string += random.toString();
     }
     
 
     return (
+        <>
+        <DS difficultyChange={difficultyChange}/>
         <Display key={key} string = {string} handleRestart = {handleRestart}/>
+        </>
     )
 }
 
